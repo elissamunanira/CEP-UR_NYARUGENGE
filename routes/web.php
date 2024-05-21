c2d5aeedb61f3d2884ec5413dff0f22a1e08dd1a
elissamunanira <munanira64@gmail.com>
2024-05-21 22:10:13 +0200

    post

--- routes/web.php
+++ routes/web.php
@@ -15,12 +15,14 @@ Route::get('/posts/create',[PostController::class,'create'])->name('post.create'
 Route::post('/posts/store', [PostController::class,'store'])->name('post.store');
 Route::get('posts/edit',[PostController::class,'edit'])->name('post.edit');
 Route::put('/posts/update',[PostController::class,'update'])->name('post.update');
+Route::delete('/posts/delete',[PostController::class,'delete'])->name('post.delete');
 
 //categories routes
 
-Route::get('categories', [CategoryController::class, 'index'])->name('categories.index');
-Route::get('category/create',[CategoryController::class, 'create'])->name('category.create');
-Route::get('category/edit',[CategoryController::class, 'edit'])->name('category.edit');
-Route::put('category/update',[CategoryController::class, 'update'])->name('category.update');
+Route::get('/categories', [CategoryController::class, 'index'])->name('categories.index');
+Route::get('/category/create',[CategoryController::class, 'create'])->name('category.create');
+Route::post('/categories/store',[CategoryController::class, 'store'])->name('category.store');
+Route::get('/category/edit',[CategoryController::class, 'edit'])->name('category.edit');
+Route::put('/category/update',[CategoryController::class, 'update'])->name('category.update');
 
 
