270693d01742fb297d6573ca1d7fa4f7b553d784
elissamunanira <munanira64@gmail.com>
2024-05-23 11:16:37 +0200

    postcontroller

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Post;
+use App\Http\Controllers\Storage;
 
 class PostController extends Controller
 {
@@ -77,16 +78,44 @@ class PostController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, Post $post)
     {
         //
+        $request->validate([
+            'title' => 'required|string|max:255',
+            'body' => 'required',
+            'cover_image' => 'nullable|required|mimes:jpeg,png,jpg,gif,svg|max:2048',
+            'user_id' => 'required',
+            'category_id' => 'required|exists:categories,id',
+        ]);
+
+        if($request->hasFile('cover_image')){
+            if($post->cover_image){
+                Storage::disk('cover_images','public')->delete($post->cover_image);
+            }
+
+            $path = $request->file('cover_image')->store('cover_images','public');
+        }
+
+        $post->update([
+            'title' => $request->title,
+            'body' => $request->body,
+            'category_id' => $request->category_id,
+            'user_id' => $request->user_id,
+            'cover_image' => $path ?? null,
+        ]);
+
+        return response()->json($post, 200);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Post $post)
     {
         //
+        $post->delete();
+        return response()->json(null, 204);
+
     }
 }
